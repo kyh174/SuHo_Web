7708826a0f9ff6547b295c7dee21215984bf4b00
kyh174 <kyh174@naver.com>
2022-04-20 21:19:02 +0900

    0420_시리얼 웹 개발중
    Co-Authored-By: hisumin46 <83199550+hisumin46@users.noreply.github.com>

--- Serial/app.js
+++ Serial/app.js
@@ -1,2 +1,17 @@
 const express = require('express');
-const app = express;
+const app = express();
+const fs = require('fs');
+
+app.set('port', 3030);
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', (req, res) => {
+  fs.readFile('./public/html/main.html', (err, data) => {
+    res.end(data);
+  });
+});
+
+app.listen(app.get('port'), () => {
+  console.log('http://localhost:' + app.get('port'));
+});
