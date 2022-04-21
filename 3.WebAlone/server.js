df08cd2f65b569d2ad5dbf25c10ad8380db711e0
kyh174 <kyh174@naver.com>
2022-04-21 20:46:16 +0900

    0421_WebAlone 정적파일 처리

--- 3.WebAlone/server.js
+++ 3.WebAlone/server.js
@@ -9,20 +9,27 @@ app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars'); // view engine 변수 설정
 
 /*
+익스프레스는 정적 파일과 뷰를 미들웨어로 처리함
+public 디렉터리 안에는 정적파일을 넣으며, 이는 특별한 조건 없이
+클라이언트에 보냄
+*/
+// public 미들웨어 처리 (정적파일 처리)
+app.use(express.static(__dirname + '/public'));
+
+
+/*
 app.get('path', callbackfunc);
 GET 메소드 요청을 지정된 경로로 라우트
 */
 
 // 루트 페이지
 app.get('/', (req, res) => {
-  res.type('text/plain');
-  res.send('Main');
+  res.render('home');
 });
 
 // about 페이지
 app.get('/about', (req, res) => {
-  res.type('text/plain');
-  res.send('About');
+  res.render('about');
 });
 
 /*
@@ -30,13 +37,13 @@ app.use('path', {callbackfunc})
 지정된 경로에 미들웨어 함수를 마운트
 요청 경로가 지정된 경로와 일치하면 미들웨어 함수 실행
 path 인수 안넘기면 기본으로 / 경로
+익스프레스는 미들웨어로 뷰와 정적 파일을 처리함
 */ 
 
 // 404 페이지 - 요청 못찾음
 app.use(function(req, res) {
-  res.type('text/plain');
   res.status(404);
-  res.send('404 - Not Found');
+  res.render('404');
 });
 
 /*
@@ -49,10 +56,11 @@ console.error(err.stack)으로 에러 출력
 // 500 페이지 - 서버에러
 app.use(function(err, req, res, next) {
   console.error(err.stack);
-  res.type('text/plain');
   res.status(500);
-  res.end('500 - Server Error');
+  res.render('500');
 });
+
+
 
 app.listen(app.get('port'), function() {
   console.log('http://localhost:' + app.get('port'));
