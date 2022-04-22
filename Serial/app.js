6c74d7c44aa55553d79d0e485e7373b238191275
kyh174 <kyh174@naver.com>
2022-04-22 11:17:26 +0900

    0422_SerialPort 웹에서 제어 성공

--- Serial/app.js
+++ Serial/app.js
@@ -22,11 +22,13 @@ app.get('/', (req, res) => {
 });
 
 app.get('/led-on', (req, res) => {
-  port.write();
+  port.write([1]);
+  res.end();
 });
 
 app.get('/led-off', (req, res) => {
-  port.write();
+  port.write([0]);
+  res.end();
 });
 
 app.listen(3030, () => {
