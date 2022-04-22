3d6b2c3be750c5aedd4d406fdb5c3cc16164e335
kyh174 <kyh174@naver.com>
2022-04-22 10:29:52 +0900

    0422_SerialPort 개발중..

--- Serial/app.js
+++ Serial/app.js
@@ -1,27 +1,34 @@
 const express = require('express');
 const app = express();
 const fs = require('fs');
+
 const { SerialPort } = require('serialport');
-const openPort = new SerialPort({ path: 'COM3', baudRate: 9600 });
+const port = new SerialPort({
+  path: 'COM6',
+  baudRate: 9600
+});
 
-app.set('port', 3030);
-
-app.use(express.static(__dirname + '/public'));
+port.on('open', (err) => {
+  if(!err)
+    console.log('Serial Port Open');
+  else
+    console.error(err);
+});
 
 app.get('/', (req, res) => {
-  fs.readFile('./public/html/main.html', (err, data) => {
+  fs.readFile('./main.html', (err, data) => {
     res.end(data);
   });
 });
 
-app.listen(app.get('port'), () => {
-  console.log('http://localhost:' + app.get('port'));
+app.get('/led-on', (req, res) => {
+  port.write();
+});
 
-  openPort.on('open', () => {  // 포트통신 열기
-    console.log('Port Open');
-  });
+app.get('/led-off', (req, res) => {
+  port.write();
+});
 
-  openPort.on('data', (data) => {  // 포트통신 보내기
-    openPort.write(data);
-  });
+app.listen(3030, () => {
+  console.log('Server Open');
 });
