c005c2ee8ec5ff0f05fa0ad1d29838a1c7eb48c1
kyh174 <kyh174@naver.com>
2022-05-22 15:57:13 +0900

    0522_일단은 프론트에서 작성한 코드 클라이언트에서 python스크립트로 실행하여 결과 가져오는거 가능

--- IoT-Project/client.js
+++ IoT-Project/client.js
@@ -1,6 +1,18 @@
 const { io } = require('socket.io-client');
 const socket = io('ws://localhost:3000');
+const { PythonShell } = require('python-shell');
+const fs = require('fs');
 
 socket.on('server send code', (code) => {
-  console.log(code);
+  fs.writeFile('./code.py', code, (err) => {
+    if (err) {
+      console.error(err);
+      return
+    }
+    PythonShell.run('code.py', null, (err, result) => {
+      if (err) throw err;
+      console.log('finished');
+      console.log('results: ' + result);
+    });
+  });
 });
