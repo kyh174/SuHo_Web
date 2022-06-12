f38269bfd1244d14cc6f6c171995cc53d92506d6
kyh174 <kyh174@naver.com>
2022-06-13 00:06:57 +0900

    0613_클라이언트파일

---
+++ RaspberryClient/client.js
@@ -0,0 +1,21 @@
+const { io } = require('socket.io-client');
+const socket = io('http://192.168.0.100:18090');
+const { PythonShell } = require('python-shell');
+const fs = require('fs');
+
+socket.on('server send code', (code) => {
+  fs.writeFile('./code.py', code, (err) => {
+    if (err) {
+      console.error(err);
+      return
+    }
+    PythonShell.run('code.py', null, (err, result) => {
+      if (err) {
+        socket.emit('client send result error', err);
+        return
+      }
+      console.log(`finished: ${result}`);
+      socket.emit('client send result', result);
+    });
+  });
+});
